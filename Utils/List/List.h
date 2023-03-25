101d0e86d105de93be2bbb304d38014d8b137081
Yingjing Xu <xuyingjing@163.com>
2023-03-25 12:04:32 +0800

    feat(Utils/List): support single list

--- Utils/List/List.h
+++ Utils/List/List.h
@@ -59,4 +59,63 @@ static inline list_node_t * list_get_next(list_node_t * entry)
     return entry->next;
 }
 
+typedef struct slist_node
+{
+    struct slist_node * next;
+} slist_node_t;
+
+/**
+ * @brief Initialize single list
+ * 
+ * @param head Head node of single list
+ */
+static inline void slist_init(slist_node_t * head)
+{
+    head->next = (slist_node_t *)0;
+}
+
+/**
+ * @brief Insert node after specified node
+ * 
+ * @param node Specified node
+ * @param entry Node to insert
+ */
+static inline void slist_add(slist_node_t * node, slist_node_t * entry)
+{
+    entry->next = node->next;
+    node->next = entry;
+}
+
+/**
+ * @brief Remove node from specified single list
+ * 
+ * @param head Head of specified single list
+ * @param entry Node to remove
+ */
+static inline void slist_del(slist_node_t * head, slist_node_t * entry)
+{
+    slist_node_t * index = head;
+
+    while ((index->next != (slist_node_t *)0) && (index->next != entry))
+    {
+        index = index->next;
+    }
+
+    if (index->next != (slist_node_t *)0)
+    {
+        index->next = index->next->next;
+    }
+}
+
+/**
+ * @brief Get next node
+ * 
+ * @param entry Specified node
+ * @return slist_node_t The next node
+ */
+static inline slist_node_t * slist_get_next(slist_node_t * entry)
+{
+    return entry->next;
+}
+
 #endif
